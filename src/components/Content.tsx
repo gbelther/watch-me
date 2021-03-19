0eb9e29d584b5b0178e655ecbd70b2394bcca013
GBelther <belthergabriel@gmail.com>
2021-03-19 09:34:37 -0300

    organizando o app em componentes

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,41 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard";
+
+interface MoviesProps {
+  movies: {
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Value: string;
+    }>;
+    Runtime: string;
+  }[];
+  selectedGenre: {
+    title: string;
+  };
+}
+
+export function Content({ movies, selectedGenre }: MoviesProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.Title}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
